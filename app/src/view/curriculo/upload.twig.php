e6cc29f76b783ba3c54f13fc091aad9111bdcc4d
mussatto7 <83254538+mussatto7@users.noreply.github.com>
2022-12-17 18:25:03 -0300

    config

--- app/src/view/curriculo/upload.twig.php
+++ app/src/view/curriculo/upload.twig.php
@@ -7,11 +7,10 @@
 <form action="/arquivo-salvar" method="post" enctype="multipart/form-data">
     <input type="hidden" name="id" value="{{id}}">
     <div class="mb-3">
-        <label for="arquivo" class="form-label">Curriculo em Arquivo</label>
+        <label for="arquivo" class="form-label">Curriculo em Arquivo1</label>
         <input type="file" name="curriculo" class="form-control" id="arquivo" aria-describedby="arquivoHelp">
         <p id="arquivoHelp" class="form-text">Envie seu Currículo.</p>
     </div>
     <button type="submit" class="btn btn-primary">ENVIAR CURRÍCULO</button>
 </form>
-
 {% endblock %}
